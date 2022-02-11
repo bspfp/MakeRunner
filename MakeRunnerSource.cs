133b91444c629a66cc54bdaf08451f47ce5c24b9
bspfp <kwanghochoi@hotmail.com>
2022-02-11 19:50:24 +0900

    실행 파라미터를 넘겨줄 수 있게 수정

--- MakeRunnerSource.cs
+++ MakeRunnerSource.cs
@@ -14,10 +14,15 @@ using System.Reflection;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 static class Runner {
+	static string appArgs;
+	
 	static void Main() {
+		InitAppArgs();
+		
 		// var psi = RunShellCommand("https://github.com/bspfp/MakeRunner", "Open");
-		// var psi = RunGUIApp("notepad.exe", "");
-		var psi = RunConsoleApp("\"D:\\RunSome.cmd\"", noWindow: true);
+		// var psi = RunGUIApp("notepad.exe", bypass: true);
+		// var psi = RunConsoleApp("\"D:\\RunSome.cmd\"", noWindow: true);
+		var psi = RunConsoleApp("dir", keepConsole: true, bypass: true);
 		
 		// psi.WorkingDirectory = "<시작 폴더>";
 		Process.Start(psi);
@@ -31,21 +36,43 @@ static class Runner {
 		};
 	}
 	
-	static ProcessStartInfo RunGUIApp(string filename, string arguments = "") {
+	static ProcessStartInfo RunGUIApp(string filename, string arguments = "", bool bypass = false) {
 		return new ProcessStartInfo() {
 			FileName = filename,
-			Arguments = arguments,
+			Arguments = arguments + (bypass ? appArgs : ""),
 			UseShellExecute = false,
 		};
 	}
 	
-	static ProcessStartInfo RunConsoleApp(string filename, string arguments = "", bool keepConsole = false, bool noWindow = false) {
+	static ProcessStartInfo RunConsoleApp(string filename, string arguments = "", bool bypass = false, bool keepConsole = false, bool noWindow = false) {
 		var cmdOpt = keepConsole ? "/K" : "/C";
 		return new ProcessStartInfo() {
 			FileName = "cmd.exe",
-			Arguments = cmdOpt + " " + filename + " " + arguments,
+			Arguments = cmdOpt + " " + filename + " " + arguments + (bypass ? appArgs : ""),
 			UseShellExecute = false,
 			CreateNoWindow = !keepConsole && noWindow
 		};
+	}
+	
+	static void InitAppArgs() {
+		appArgs = Environment.CommandLine;
+		var startIndex = -1;
+		if (appArgs.StartsWith("\"")) {
+			var endName = appArgs.Substring(1).IndexOf('"');
+			if (endName != -1)
+				startIndex = endName + 2;
+		}
+		else {
+			for (var i = 1; i < appArgs.Length; i++) {
+				if (char.IsWhiteSpace(appArgs[i])) {
+					startIndex = i;
+					break;
+				}
+			}
+		}
+		if (startIndex != -1)
+			appArgs = appArgs.Substring(startIndex);
+		else
+			appArgs = "";
 	}
 }
